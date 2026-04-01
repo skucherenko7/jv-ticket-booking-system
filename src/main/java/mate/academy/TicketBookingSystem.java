6a8be6da7071ec720a42de7ba256a3a9f5e04081
svit7 <svit7kucherenko@gmail.com>
2026-04-01 12:42:45 +0300

    HW "Online ticket booking system"

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,24 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private final Semaphore seatsSemaphore;
 
+    public TicketBookingSystem(int totalSeats) {
+        this.seatsSemaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (seatsSemaphore.tryAcquire()) {
+            return new BookingResult(user, true, "Booking confirmed for user:" + user);
+        } else {
+            return new BookingResult(user, false, "No seats available for user: " + user);
+        }
+    }
+
+    public void cancelBooking() {
+        seatsSemaphore.release();
     }
 }
